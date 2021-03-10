272267111ed9eb87c01ff50d96b80c511ad04ca3
wemjii <60511633+wemjii@users.noreply.github.com>
2021-03-10 10:46:30 -0500

    lut implemented

--- tutorial.py
+++ tutorial.py
@@ -1,6 +1,7 @@
 import pci
 from pci.ihs import ihs
 from pci.fun import fun
+from pci.lut import lut
 # print("This version of PCI is", pci.version)
 # print(ihs)
 
@@ -16,7 +17,7 @@ ihs(file, dbic, dboc, dbiw, ihsmodel)
 func = "MATC" # Enhancement function
 dbic = [1] # Input raster channel(s)
 dblut = []
-dbsn = ""
+dbsn = "Testing"
 dbsd = "SPOT Matched to Intensity" # Output LUT segment description
 ostr = []
 sdpt = []
@@ -26,3 +27,17 @@ dbhc = [6] #Input histogram match channel
 lasc = []
 
 fun(file, func, dbic, dblut, dbsn, dbsd, ostr, sdpt, trim, mask, dbhc, lasc)
+
+fili = file
+dbic = [1]
+lutfile = file # Input file name
+dblut = [3] # This should match the lut file created
+maskfile = ""
+mask = []
+filo = fili
+dboc = [9]
+datatype = ""
+ftype = ""
+foptions = ""
+
+lut(fili, dbic, lutfile, dblut, maskfile, mask, filo, dboc, datatype, ftype, foptions)
